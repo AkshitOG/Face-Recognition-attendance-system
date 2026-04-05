1d6d950ba009a290c5d3a9cf353cfbd6b40d637b
AkshitOG <akshitsaxena23@gmail.com>
2026-04-05 12:13:30 +0530

    Adding Face Detection using Opencv Haarcascade pretrained model

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
 from flask import Flask, request, render_template, Response
 # from flask import SQLAlchemy
-# import os
+import os
 import cv2
-
 
 app = Flask(__name__)
 
+camera = cv2.VideoCapture(0)
+
+#https://github.com/opencv/opencv/tree/4.x/data/haarcascades
+cascade_path = os.path.join(
+    app.root_path,
+    'static',
+    'cascades',
+    'haarcascade_frontalface_default.xml'
+)
+face_cascade = cv2.CascadeClassifier(cascade_path)
+
 def camera_on():
-    camera = cv2.VideoCapture(0)
     while True:
         success, frame = camera.read()
         if not success:
             break
-        else:
-            frame = cv2.flip(frame, 1)
-            ret, buffer = cv2.imencode('.jpg',frame)
-            frame = buffer.tobytes()
-            
-            yield ((b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'))
+        
+        frame = cv2.flip(frame, 1)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w_box,h_box) in faces:
+            cv2.rectangle(frame, (x,y), (x+w_box, y+h_box), (0,255,0), 2)
+        
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()            
+
+        yield ((b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'))
 
 @app.route('/video')
 def video():
